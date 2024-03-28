f810d5afb1bb74a663e5866408df66ac82417d3f
DominicHuynh <phucvbin2005@gmail.com>
2024-03-28 15:09:42 +1030

    review

---
+++ StoreShelf.cpp
@@ -0,0 +1,52 @@
+
+#include "StoreShelf.h"
+#include "MusicBox.h"
+StoreShelf::StoreShelf() : width(0), num_music_boxes(0), contents(nullptr) {}
+
+StoreShelf::StoreShelf(int width) : width(width), num_music_boxes(0), contents(nullptr) {}
+
+int StoreShelf::get_width() {
+    return width;
+}
+
+int StoreShelf::get_num_music_boxes() {
+    return num_music_boxes;
+}
+
+MusicBox* StoreShelf::get_contents() {
+    return contents;
+}
+
+bool StoreShelf::add_music_box(MusicBox a_music_box) {
+    if (contents == nullptr) {
+        contents = new MusicBox[1];
+        contents[0] = a_music_box;
+        num_music_boxes++;
+        return true;
+    }
+    else {
+        int total_width = 0;
+        for (int i = 0; i < num_music_boxes; i++) {
+            total_width += contents[i].get_width();
+        }
+        total_width += a_music_box.get_width();
+        if (total_width <= width) {
+            MusicBox *temp = new MusicBox[num_music_boxes + 1];
+            for (int i = 0; i < num_music_boxes; i++) {
+                temp[i] = contents[i];
+            }
+            temp[num_music_boxes] = a_music_box;
+            delete[] contents;
+            contents = temp;
+            num_music_boxes++;
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+}
+
+StoreShelf::~StoreShelf() {
+    delete[] contents;
+}
---
+++ StoreShelf.h
@@ -0,0 +1,23 @@
+
+#ifndef STORESHELF_H
+#define STORESHELF_H
+
+#include "MusicBox.h"
+
+class StoreShelf {
+public:
+    StoreShelf(); // default constructor
+    StoreShelf(int width); // constructor with width parameter
+    int get_width(); // get shelf width
+    int get_num_music_boxes(); // get number of music boxes
+    MusicBox* get_contents(); // get array of music boxes
+    bool add_music_box(MusicBox a_music_box); // add music box to shelf
+    ~StoreShelf(); // destructor
+
+private:
+    int width;
+    int num_music_boxes;
+    MusicBox *contents;
+};
+
+#endif
